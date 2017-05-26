6d5b0eca81d237393450f5cc7e97409d42d0e025
dufei <dufeizj@163.com>
2017-05-26 22:25:37 +0800

    语料模型的测试实例

---
+++ src/test/java/CorpusTest.java
@@ -0,0 +1,35 @@
+import org.hfutec.nlp.model.Corpus;
+
+/**
+ * 读取语料
+ * Created by DuFei on 2017/5/26.
+ */
+public class CorpusTest {
+
+  public static void main(String[] args) {
+
+    String inputFile = "d:/test.txt";
+
+    //读取文件并保存语料
+    Corpus corpus = new Corpus(inputFile, false);
+    System.out.println(corpus.docSize);
+    System.out.println(corpus.wordSize);
+    System.out.println(corpus.vocabularySize);
+    System.out.println(corpus.wordIndex);
+
+    corpus.saveCorpus(inputFile);
+
+
+    //载入之前保存的语料
+    Corpus corpusLoading = new Corpus();
+    corpusLoading.loadCorpus(inputFile);
+
+    //输入是文件夹的测试，去掉false参数即可
+    String inputDir = "D:/test";
+    String outputDir = "d:/test_out";
+    Corpus corpusDir = new Corpus(inputDir);
+    corpusDir.saveCorpus(outputDir);
+
+  }
+
+}
